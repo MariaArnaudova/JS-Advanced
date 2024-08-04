afac69a3ee3c33f01fdd3f49a98786e4e43d3efb
MariaCode <m.p.arnaudova@gmail.com>
2024-08-04 10:11:40 +0300

    Added solution

---
+++ 5. Unit Testing and Error Handling_Exe/07. JS-Advanced-Unit-Testing-and-Error-Handling-Exercise-Resources/03.charLookup.js
@@ -0,0 +1,40 @@
+const expect = require('chai').expect;
+
+function lookupChar(string, index) {
+    if (typeof (string) !== 'string' || !Number.isInteger(index)) {
+        return undefined;
+    }
+    if (string.length <= index || index < 0) {
+        return "Incorrect index";
+    }
+
+    return string.charAt(index);
+}
+
+
+
+describe('get character of index', () => {
+    it('check whether first parameter is valid', () => {
+        expect(lookupChar(0, 0)).to.equal(undefined);
+    })
+
+    it('check whether second parameter is valid', () => {
+        expect(lookupChar('hello', 'test')).to.equal(undefined);
+    })
+
+    it('check whether second parameter is valid integer', () => {
+        expect(lookupChar('hello', 0.1)).to.equal(undefined);
+    })
+
+    it('check value with index bigger than string length', () => {
+        expect(lookupChar('hi', 10)).to.equal('Incorrect index');
+    })
+
+    it('check value with negative index', () => {
+        expect(lookupChar('hi', -10)).to.equal('Incorrect index');
+    })
+
+    it('check value with valid parameters', () => {
+        expect(lookupChar('hello', 1)).to.equal('e');
+    })
+})
